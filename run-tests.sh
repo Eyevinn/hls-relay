1bf25af89422182ef2f4545b341c5377873ec10a
birme <jonas.birme@eyevinn.se>
2016-09-10 08:57:15 +0200

    Error in return code for tests

--- run-tests.sh
+++ run-tests.sh
@@ -4,15 +4,15 @@ function install_deps {
 }
 
 function run {
-    python tests/mockserver.py &
     PYTHONPATH=. py.test -vv --cov-report term-missing --cov hlsrelay tests/
-    ps ax | grep "python tests/mockserver.py" | grep -v grep | cut -d ' ' -f 1 | xargs kill
 }
 
 function main {
     install_deps
+    python tests/mockserver.py &
     run
     retval=$?
+    ps ax | grep "python tests/mockserver.py" | grep -v grep | cut -d ' ' -f 1 | xargs kill
     return "$retval"
 }
 
